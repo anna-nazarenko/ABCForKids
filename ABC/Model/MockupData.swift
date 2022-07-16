338951d7e6d1da305b24a2a7d906c2a5ebc02937
olgazinchenko <ozinchenko.pm@gmail.com>
2022-07-16 13:11:13 +0200

    Updated LetterCard identifiers in the MockupData file

--- ABC/Model/MockupData.swift
+++ ABC/Model/MockupData.swift
@@ -9,17 +9,17 @@ import Foundation
 import UIKit
 
 let mockupData = [
-    LetterCard(identifier: 1, letter: "A", words: [
+    LetterCard(identifier: 0, letter: "A", words: [
         Word(word: "Апельсин", image: UIImage(named: "letter-a-ru")!),
         Word(word: "Абрикос", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Акула", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 2, letter: "Б", words: [
+    LetterCard(identifier: 1, letter: "Б", words: [
         Word(word: "Бабочка", image: UIImage(named: "letter-b-ru")!),
         Word(word: "Бинокль", image: UIImage(systemName: "leaf.circle")!),
         Word(word: "Бобы", image: UIImage(systemName: "leaf.circle")!),
     ]),
-    LetterCard(identifier: 3, letter: "В", words: [
+    LetterCard(identifier: 2, letter: "В", words: [
         Word(word: "Волк", image: UIImage(named: "letter-v-ru")!),
         Word(word: "Винигрет", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Ворон", image: UIImage(systemName: "flame")!),
@@ -29,139 +29,139 @@ let mockupData = [
         Word(word: "Гнездо", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Глаз", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Д", words: [
+    LetterCard(identifier: 4, letter: "Д", words: [
         Word(word: "Дерево", image: UIImage(named: "letter-d-ru")!),
         Word(word: "Дом", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Дрова", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Е", words: [
+    LetterCard(identifier: 5, letter: "Е", words: [
         Word(word: "Ежевика", image: UIImage(named: "letter-e-ru")!),
         Word(word: "Ершик", image: UIImage(systemName: "ladybug.fill")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ё", words: [
+    LetterCard(identifier: 6, letter: "Ё", words: [
         Word(word: "Ёлка", image: UIImage(named: "letter-jo-ru")!),
         Word(word: "Ёж", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Ёрш", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ж", words: [
+    LetterCard(identifier: 7, letter: "Ж", words: [
         Word(word: "Жираф", image: UIImage(named: "letter-zh-ru")!),
         Word(word: "Жаба", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Жук", image: UIImage(systemName: "clock.badge.checkmark.fill")!),
     ]),
-    LetterCard(identifier: 3, letter: "З", words: [
+    LetterCard(identifier: 9, letter: "З", words: [
         Word(word: "Зебра", image: UIImage(named: "letter-z-ru")!),
         Word(word: "Зонт", image: UIImage(systemName: "leaf.circle")!),
         Word(word: "Зима", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "И", words: [
+    LetterCard(identifier: 10, letter: "И", words: [
         Word(word: "Игла", image: UIImage(named: "letter-i-ru")!),
         Word(word: "Игрушки", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Икра", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Й", words: [
+    LetterCard(identifier: 11, letter: "Й", words: [
         Word(word: "Йогурт", image: UIImage(named: "letter-j-ru")!),
         Word(word: "Йог", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Йод", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "К", words: [
+    LetterCard(identifier: 12, letter: "К", words: [
         Word(word: "Колесо", image: UIImage(named: "letter-k-ru")!),
         Word(word: "Кнопка", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Кукла", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Л", words: [
+    LetterCard(identifier: 13, letter: "Л", words: [
         Word(word: "Лимон", image: UIImage(named: "letter-l-ru")!),
         Word(word: "Лекарство", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Лес", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "М", words: [
+    LetterCard(identifier: 14, letter: "М", words: [
         Word(word: "Медведь", image: UIImage(named: "letter-m-ru")!),
         Word(word: "Мама", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Мёд", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Н", words: [
+    LetterCard(identifier: 15, letter: "Н", words: [
         Word(word: "Ножницы", image: UIImage(named: "letter-n-ru")!),
         Word(word: "Нока", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Нота", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "О", words: [
+    LetterCard(identifier: 16, letter: "О", words: [
         Word(word: "Обезьяна", image: UIImage(named: "letter-o-ru")!),
         Word(word: "Обрако", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Окно", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "П", words: [
+    LetterCard(identifier: 17, letter: "П", words: [
         Word(word: "Петух", image: UIImage(named: "letter-p-ru")!),
         Word(word: "Пряник", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Паровоз", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Р", words: [
+    LetterCard(identifier: 18, letter: "Р", words: [
         Word(word: "Рыба", image: UIImage(named: "letter-r-ru")!),
         Word(word: "Река", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Рот", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "С", words: [
+    LetterCard(identifier: 19, letter: "С", words: [
         Word(word: "Солнце", image: UIImage(named: "letter-s-ru")!),
         Word(word: "Свинья", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Ставок", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Т", words: [
+    LetterCard(identifier: 20, letter: "Т", words: [
         Word(word: "Тигр", image: UIImage(named: "letter-t-ru")!),
         Word(word: "Трава", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Трон", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "У", words: [
+    LetterCard(identifier: 21, letter: "У", words: [
         Word(word: "Утка", image: UIImage(named: "letter-u-ru")!),
         Word(word: "Улыбка", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Ухо", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ф", words: [
+    LetterCard(identifier: 22, letter: "Ф", words: [
         Word(word: "Фея", image: UIImage(named: "letter-f-ru")!),
         Word(word: "Фонарь", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Фазан", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Х", words: [
+    LetterCard(identifier: 23, letter: "Х", words: [
         Word(word: "Хлеб", image: UIImage(named: "letter-h-ru")!),
         Word(word: "Хомяк", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Хищник", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ц", words: [
+    LetterCard(identifier: 24, letter: "Ц", words: [
         Word(word: "Цветок", image: UIImage(named: "letter-c-ru")!),
         Word(word: "Цапля", image: UIImage(systemName: "ladybug.fill")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ч", words: [
+    LetterCard(identifier: 25, letter: "Ч", words: [
         Word(word: "Часы", image: UIImage(named: "letter-ch-ru")!),
         Word(word: "Чайка", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Чашка", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ш", words: [
+    LetterCard(identifier: 26, letter: "Ш", words: [
         Word(word: "Шарик", image: UIImage(named: "letter-sh-ru")!),
         Word(word: "Шоколад", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Штаны", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Щ", words: [
+    LetterCard(identifier: 27, letter: "Щ", words: [
         Word(word: "Щётка", image: UIImage(named: "letter-shh-ru")!),
         Word(word: "Щенок", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Щетиина", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ъ", words: [
+    LetterCard(identifier: 28, letter: "Ъ", words: [
         Word(word: "Объявление", image: UIImage(named: "letter-tverdyj-znak-ru")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ы", words: [
+    LetterCard(identifier: 29, letter: "Ы", words: [
         Word(word: "Сыр", image: UIImage(named: "letter-y-ru")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ь", words: [
+    LetterCard(identifier: 30, letter: "Ь", words: [
         Word(word: "Мышь", image: UIImage(named: "letter-mjagkij-znak-ru")!),
     ]),
-    LetterCard(identifier: 3, letter: "Э", words: [
+    LetterCard(identifier: 31, letter: "Э", words: [
         Word(word: "Эскаватор", image: UIImage(named: "letter-je-ru")!),
         Word(word: "Эльбрус", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Экватор", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Ю", words: [
+    LetterCard(identifier: 32, letter: "Ю", words: [
         Word(word: "Юбка", image: UIImage(named: "letter-ju-ru")!),
         Word(word: "Юшка", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Юла", image: UIImage(systemName: "flame")!),
     ]),
-    LetterCard(identifier: 3, letter: "Я", words: [
+    LetterCard(identifier: 33, letter: "Я", words: [
         Word(word: "Яблоко", image: UIImage(named: "letter-ja-ru")!),
         Word(word: "Ящик", image: UIImage(systemName: "ladybug.fill")!),
         Word(word: "Яхта", image: UIImage(systemName: "flame")!),
